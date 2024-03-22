74d3ada58a4b26660529a33d573b71a7d4b197b0
Miguel Angel Ramos <return.infox@gmail.com>
2024-03-22 20:08:20 -0300

    primera propuesta de menu

---
+++ main.py
@@ -0,0 +1,37 @@
+def menu_principal():
+    while True:
+        print("\n Menu principal de la Tienda de VideoJuegos")
+        print("1. Registrar Desarrolladora")
+        print("2. Registrar VideoJuego")
+        print("3. Registrar Empleado")
+        print("4. Registrar Cliente")
+        print("5. Registrar Venta")
+        print("6. Actualizar Precio de un VideoJuego")
+        print("7. Recargar Saldo del Cliente")
+        print("8. Salir")
+
+        opcion = input("Seleccione una opción: ")
+        if opcion == "1":
+            pass
+        elif opcion == "2":
+            pass
+        elif opcion == "3":
+            pass
+        elif opcion == "4":
+            pass
+        elif opcion == "5":
+            pass
+        if opcion == "6":
+            pass
+        if opcion == "7":
+            pass
+        if opcion == "8":
+            print("Saliendo del Programa....")
+            break
+        else:
+            print("Opción no válida, intente de nuevo")
+
+
+
+if __name__ == '__main__':
+    menu_principal()
