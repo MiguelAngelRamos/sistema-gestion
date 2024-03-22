f8a4b9ff873acd84cf1ac8ee6b9cba82fc16d1ee
Miguel Angel Ramos <return.infox@gmail.com>
2024-03-22 19:57:45 -0300

    el codigo del empleado

--- modules/empleado.py
+++ modules/empleado.py
@@ -5,6 +5,19 @@ class Empleado:
         self.comision = comision
 
     def vender(self, videojuego, cliente):
-        if cliente.saldo
+        if cliente.saldo < videojuego.precio:
+            return "Venta no realiza: saldo insuficiente"
+        if videojuego.stock <= 0:
+            return "Venta no realiza: stock insuficiente"
+        videojuego.stock -= 1
+        # 1000 * 0.05 = 50
+        self.comision += videojuego.precio * 0.05
+        cliente.saldo -= videojuego.precio
+        return f"Venta realizada. Comisión del empleado actualizada : {self.comision}. Saldo del Cliente: {cliente.saldo}."
+
+
+
+
+
 
 
