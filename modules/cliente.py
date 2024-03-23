f2cdd2c00d1f88872cd45ff4085f6b617d9a2254
Miguel Angel Ramos <return.infox@gmail.com>
2024-03-22 21:04:59 -0300

    imprimir por tabla los clientes

--- modules/cliente.py
+++ modules/cliente.py
@@ -1,3 +1,6 @@
+from prettytable import PrettyTable
+
+
 class Cliente:
     def __init__(self, id_cliente, nombre, saldo, preferencia_videojuego=None):
         self.id_cliente = id_cliente
@@ -12,13 +15,26 @@ class Cliente:
 
 dic_cliente = {}
 
+
 def registrar_cliente():
     print("\n Registrar un Nuevo Cliente")
     id_cliente = input("ID: ")
     nombre = input("Nombre: ")
     saldo = float(input("Saldo: "))
     preferencia_videojuego = input("Preferencia de video juego (opcional): ")
-    cliente = Cliente(id_cliente, nombre, saldo, preferencia_videojuego)
-    dic_cliente[id_cliente] = cliente
-    print("Cliente registrado exitosamente")
-    print(dic_cliente)
+
+    try:
+        cliente = Cliente(id_cliente, nombre, saldo, preferencia_videojuego)
+        dic_cliente[id_cliente] = cliente
+        print("Cliente registrado exitosamente")
+    except ValueError as error:
+        print(f"Error al registrar el cliente: {error}")
+
+
+def imprimir_clientes():
+    tabla = PrettyTable()
+    tabla.field_names = ["ID", "Nombre", "Saldo", "Preferencia Juego"]
+    for obj_cliente in dic_cliente.values():
+        tabla.add_row([obj_cliente.id_cliente, obj_cliente.nombre, obj_cliente.saldo, obj_cliente.preferencia_videojuego])
+        print(tabla)
+
